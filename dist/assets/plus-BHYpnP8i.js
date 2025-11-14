8958c39747a121d7a5f8c617873c8a5d463d2811
John Allen Berty <johnallenberty95@gmail.com>
2025-11-14 15:05:20 +0530

    feat: add new components for search, skeleton, table, and transactions
    - Created a search icon component in `search-BQi7r64N.js`.
    - Added a skeleton loading component in `skeleton-CxZLnZh4.js`.
    - Implemented a table component with various subcomponents in `table-CnnrqGGu.js`.
    - Developed a transactions management component in `transactions-DITexBgW.js` with filtering and pagination.
    - Introduced a custom hook for managing document title in `useMeta-RQawy8vu.js`.

--- dist/assets/plus-BHYpnP8i.js
+++ dist/assets/plus-BHYpnP8i.js
@@ -1,6 +1,6 @@
-import{C as t}from"./index-DnKD0KXe.js";/**
+import{m as e}from"./index-C7wzcO-q.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],s=t("plus",o);export{s as t};
+ */const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],t=e("plus",o);export{t as P};
--- dist/assets/skeleton-mX7TdaWE-Cq_BwO7K.js
+++
@@ -1,1 +0,0 @@
-import{x as s,l as t}from"./index-DnKD0KXe.js";function n({className:e,...a}){return s.jsx("div",{"data-slot":"skeleton",className:t("bg-accent animate-pulse rounded-md",e),...a})}export{n as o};
