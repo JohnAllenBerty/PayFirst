702cf8a442f67b2c8f9cc7e055c4b698868b9fdb
John Allen Berty <johnallenberty95@gmail.com>
2025-11-17 09:20:19 +0530

    build: deploy latest bundle to docs

--- docs/assets/search-BelX3WgQ.js
+++ docs/assets/search-BelX3WgQ.js
@@ -1,4 +1,4 @@
-import{m as c}from"./index-CRXOfsii.js";/**
+import{m as c}from"./index-CDScM0mS.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
